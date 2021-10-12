c48a6c6ad3cef40bfd24babe0fd43c035eaf0d26
Kirill <kirill.lyskov@gmail.com>
2021-10-12 16:55:41 +0300

    deleted

--- by/epamtc/lyskovkirill/task0101/util/SqrLastDigit.java
+++
@@ -1,8 +0,0 @@
-package by.epamtc.lyskovkirill.task0101.util;
-
-public class SqrLastDigit {
-
-    public static int calculateSqrLastDigit(int number) {
-        return (int)Math.pow(number % 10, 2) % 10;
-    }
-}
